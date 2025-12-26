79bd3cfb083f53464b2d528554a481498b6a0dc4
jhham0322 <ham0322@gmail.com>
2025-12-27 06:26:13 +0900

    Fix APK install button and ADB command execution

--- WindowUI/FlutterControlPanel/obj/Release/net8.0-windows/FlutterControlPanel.AssemblyInfo.cs
+++ WindowUI/FlutterControlPanel/obj/Release/net8.0-windows/FlutterControlPanel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlutterControlPanel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60c3c29055b322c4b10aa04a22881350773b0178")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd21b409ffc36e32475abf90c2ac4783f375c1c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlutterControlPanel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlutterControlPanel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
