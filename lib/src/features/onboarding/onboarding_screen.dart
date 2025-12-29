6dfdc1da50054cd75d22727d795065d0d92c40bf
jhham0322 <ham0322@gmail.com>
2025-12-30 02:04:52 +0900

    Fix: Use transparent heart icon for onboarding

--- lib/src/features/onboarding/onboarding_screen.dart
+++ lib/src/features/onboarding/onboarding_screen.dart
@@ -262,7 +262,7 @@ class _OnboardingScreenState extends ConsumerState<OnboardingScreen> {
             child: ClipRRect(
               borderRadius: BorderRadius.circular(24),
               child: Image.asset(
-                'assets/icons/icon-512.png',
+                'assets/icons/onboarding_heart.png',
                 width: 160,
                 height: 160,
                 fit: BoxFit.cover,
